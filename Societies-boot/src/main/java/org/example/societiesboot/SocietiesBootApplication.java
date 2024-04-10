68456403b33dff788e11a9fb0094cf5d18bb96bf
ljq <qing2714312353@126.com>
2024-04-10 22:35:02 +0800

    项目开始做

---
+++ Societies-boot/src/main/java/org/example/societiesboot/SocietiesBootApplication.java
@@ -0,0 +1,15 @@
+package org.example.societiesboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietiesBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocietiesBootApplication.class, args);
+
+        System.out.println("学生社团管理系统启动成功!!!!!!!!");
+    }
+
+}
---
+++ Societies-boot/src/test/java/org/example/societiesboot/SocietiesBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.societiesboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocietiesBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
